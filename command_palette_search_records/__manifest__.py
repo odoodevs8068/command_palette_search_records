98e350fca5b01ae3960730757f469081a7a81d0a
Jagadish-magesh-damad <jagadish.magesh@damad.com>
2026-09-17 10:02:14 +0300

    JD

---
+++ command_palette_search_records/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Command palette Record Search",
+    'version': '17.0',
+    'category': 'Tools',
+    'license': "AGPL-3",
+    'depends': [ 'base', 'web'],
+    'data' : [
+        'views/views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            "command_palette_search_records/static/src/js/command_palette.js",
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/assets/screenshots/banner.png'],
+    'icon': "/command_palette_search_records/static/description/icon.png",
+}
